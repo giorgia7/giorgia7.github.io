3eeb9a038e5461a0822ee54559525409d2a17ab9
giorgia7 <39789379+giorgia7@users.noreply.github.com>
2018-06-13 21:45:01 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,31 @@
+	<?php
+	include "footer.php"
+	?>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1 style="text-align:center;">La storia del cane</h1>
+					<h2 style="text-align:center;"> </h2>
+						<p>ll cane è un mammifero appartenente all'ordine Carnivora, della famiglia dei canidi. 
+						Con l'avvento dell'addomesticazione si è distinto dal lupo, del quale rappresenta una forma 
+						neotenica.Rispetto al lupo, ha canini meno aguzzi, zanne bianche, zampe più estese, intestino 
+						più lungo ed è privo di artigli affilati.
+						L'addomesticazione del cane da parte dell'uomo ha origini antichissime,
+						e risalgono a 11.000-12.000 anni fa; ma si suppone che l'origine del 
+						rapporto fra le due specie si collochi molto più indietro nel tempo, fra 19.000 e 
+						36.000 anni fa. Nel 2001 la popolazione stimata di cani era di 400 milioni.
+						</p>
+					</div>
+				</div>
+			</div>
+				<nav>
+					<ul>
+						<li><a href="Pastore Australiano.html">Pastore Australiano</a></li>
+						<li><a href="Golden Retriever.html">Golden Retriever</a></li>
+						<li><a href="Border Collie.html">Border Collie</a></li>
+					</ul>
+				</nav> 
+				<hr>
+        <footer>Tratto da Wikipedia: Le razze dei cani - Wikipedia</footer>
+	</body>
+</html>
---
+++ header.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+  <head>
+	<title>Razze di cani</title>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="css/bootstrap-reboot.min.css">
+	<link rel="stylesheet" href="css/bootstrap-grid.min.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	
+	 <!-- CSS interno -->
+    <style type="text/css">
+		body {background-color: powderblue;}
+		h1 {color:red;}
+		a:link {
+    color: red;
+    background-color: transparent;
+    text-decoration: none;
+	}
+	</style>
+
+	<!-- Custom CSS -->
+	<link rel="stylesheet" href="css/custom.css">
+	</head>
